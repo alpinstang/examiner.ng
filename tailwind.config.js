39c92c62e1adc6d1fd86b0f9d0d65b9f77a00b43
John McDonald <jmcdonald1991@gmail.com>
2021-02-19 13:16:01 -0600

    colos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      50: "#e0fff3",
+      100: "#c7ffe9",
+      200: "#75ffc8",
+      300: "#00f090",
+      400: "#00cc7a",
+      500: "#00a865",
+      600: "#008550",
+      700: "#00663d",
+      800: "#005734",
+      900: "#004d2e",
+      primary: "#008550",
+      secondary: "#00cc7a",
+      warning: "#ffd000",
+      error: "#b11629",
+      info: "#c7ffe9",
+    },
     extend: {},
   },
   variants: {
