ec719775f9e0f80645f2c2f5cb59845d32b0b13e
John McDonald <jmcdonald1991@gmail.com>
2021-02-24 04:52:25 -0600

    csv get exam names

--- cms/src/App.tsx
+++ cms/src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import {
-	AdditionalView,
-	Authenticator,
-	buildCollection,
-	buildSchema,
-	CMSApp,
-	EntityCollection,
-	EntitySaveProps,
-	ExtraActionsParams,
-	SchemaResolver,
+  AdditionalView,
+  Authenticator,
+  buildCollection,
+  buildSchema,
+  CMSApp,
+  EntityCollection,
+  EntitySaveProps,
+  ExtraActionsParams,
+  SchemaResolver,
 } from "@camberi/firecms";
 import firebase from "firebase";
 
@@ -16,9 +16,9 @@ import { firebaseConfig } from "./firebase_config";
 import { SampleExtraActions } from "./SampleExtraActions";
 import logo from "./images/logo.png";
 import {
-	blogSearchDelegate,
-	productsSearchDelegate,
-	usersSearchDelegate,
+  blogSearchDelegate,
+  productsSearchDelegate,
+  usersSearchDelegate,
 } from "./algolia_utils";
 
 import { blogSchema } from "./schemas/blog/blogSchema";
@@ -33,196 +33,196 @@ import { questionSchema } from "./schemas/exam/questionSchema";
 import { UploadQuestionsView } from "./FileUploadView";
 
 function App() {
-	productSchema.onPreSave = ({
-		schema,
-		collectionPath,
-		id,
-		values,
-		status,
-	}: EntitySaveProps<typeof productSchema>) => {
-		values.uppercase_name = values.name.toUpperCase();
-		return values;
-	};
+  productSchema.onPreSave = ({
+    schema,
+    collectionPath,
+    id,
+    values,
+    status,
+  }: EntitySaveProps<typeof productSchema>) => {
+    values.uppercase_name = values.name.toUpperCase();
+    return values;
+  };
 
-	productSchema.onSaveSuccess = (props) => {
-		console.log("onSaveSuccess", props);
-	};
+  productSchema.onSaveSuccess = (props) => {
+    console.log("onSaveSuccess", props);
+  };
 
-	productSchema.onDelete = (props) => {
-		console.log("onDelete", props);
-	};
+  productSchema.onDelete = (props) => {
+    console.log("onDelete", props);
+  };
 
-	testEntitySchema.onPreSave = ({
-		schema,
-		collectionPath,
-		id,
-		values,
-		status,
-	}: EntitySaveProps<typeof testEntitySchema>) => {
-		console.log("custom onPreSave");
-		if (!values.empty_string) values.empty_string = "";
-		return values;
-	};
+  testEntitySchema.onPreSave = ({
+    schema,
+    collectionPath,
+    id,
+    values,
+    status,
+  }: EntitySaveProps<typeof testEntitySchema>) => {
+    console.log("custom onPreSave");
+    if (!values.empty_string) values.empty_string = "";
+    return values;
+  };
 
-	const productExtraActionBuilder = ({
-		view,
-		selectedEntities,
-	}: ExtraActionsParams) => {
-		return <SampleExtraActions selectedEntities={selectedEntities} />;
-	};
+  const productExtraActionBuilder = ({
+    view,
+    selectedEntities,
+  }: ExtraActionsParams) => {
+    return <SampleExtraActions selectedEntities={selectedEntities} />;
+  };
 
-	const localeCollection: EntityCollection<
-		typeof localeSchema
-	> = buildCollection({
-		name: "Locales",
-		relativePath: "locales",
-		deleteEnabled: true,
-		schema: localeSchema,
-		defaultSize: "l",
-	});
-	const productsCollection = buildCollection({
-		relativePath: "products",
-		schema: productSchema,
-		name: "Products",
-		group: "Store",
-		textSearchDelegate: productsSearchDelegate,
-		additionalColumns: [productAdditionalColumn],
-		extraActions: productExtraActionBuilder,
-		subcollections: [localeCollection],
-		excludedProperties: ["images", "related_products"],
-		filterableProperties: ["price", "available_locales"],
-	});
+  const localeCollection: EntityCollection<
+    typeof localeSchema
+  > = buildCollection({
+    name: "Locales",
+    relativePath: "locales",
+    deleteEnabled: true,
+    schema: localeSchema,
+    defaultSize: "l",
+  });
+  const productsCollection = buildCollection({
+    relativePath: "products",
+    schema: productSchema,
+    name: "Products",
+    group: "Store",
+    textSearchDelegate: productsSearchDelegate,
+    additionalColumns: [productAdditionalColumn],
+    extraActions: productExtraActionBuilder,
+    subcollections: [localeCollection],
+    excludedProperties: ["images", "related_products"],
+    filterableProperties: ["price", "available_locales"],
+  });
 
-	const usersCollection = buildCollection<typeof usersSchema>({
-		relativePath: "users",
-		schema: usersSchema,
-		name: "Users",
-		group: "Testing",
-		textSearchDelegate: usersSearchDelegate,
-		properties: ["first_name", "last_name", "email", "phone", "picture"],
-	});
+  const usersCollection = buildCollection<typeof usersSchema>({
+    relativePath: "users",
+    schema: usersSchema,
+    name: "Users",
+    group: "Testing",
+    textSearchDelegate: usersSearchDelegate,
+    properties: ["first_name", "last_name", "email", "phone", "picture"],
+  });
 
-	const blogCollection = buildCollection({
-		relativePath: "blog",
-		schema: blogSchema,
-		name: "Blog",
-		group: "Content",
-		textSearchDelegate: blogSearchDelegate,
-		properties: [
-			"name",
-			"images",
-			"status",
-			"reviewed",
-			"products",
-			"gold_text",
-			"long_text",
-		],
-		filterableProperties: ["name", "status"],
-		initialFilter: {
-			status: ["==", "published"],
-		},
-	});
+  const blogCollection = buildCollection({
+    relativePath: "blog",
+    schema: blogSchema,
+    name: "Blog",
+    group: "Content",
+    textSearchDelegate: blogSearchDelegate,
+    properties: [
+      "name",
+      "images",
+      "status",
+      "reviewed",
+      "products",
+      "gold_text",
+      "long_text",
+    ],
+    filterableProperties: ["name", "status"],
+    initialFilter: {
+      status: ["==", "published"],
+    },
+  });
 
-	const questionCollection: EntityCollection<
-		typeof questionSchema
-	> = buildCollection({
-		name: "Questions",
-		relativePath: "questions",
-		deleteEnabled: true,
-		schema: questionSchema,
-		defaultSize: "l",
-	});
-	const examCollection = buildCollection({
-		relativePath: "exams",
-		schema: examSchema,
-		name: "Exams",
-		group: "Testing",
-		subcollections: [questionCollection],
-		excludedProperties: ["images", "available", "questions", "uppercase_name"],
-		filterableProperties: ["price", "category"],
-	});
+  const questionCollection: EntityCollection<
+    typeof questionSchema
+  > = buildCollection({
+    name: "Questions",
+    relativePath: "questions",
+    deleteEnabled: true,
+    schema: questionSchema,
+    defaultSize: "l",
+  });
+  const examCollection = buildCollection({
+    relativePath: "exams",
+    schema: examSchema,
+    name: "Exams",
+    group: "Testing",
+    subcollections: [questionCollection],
+    excludedProperties: ["images", "available", "questions", "uppercase_name"],
+    filterableProperties: ["price", "category"],
+  });
 
-	const navigation: EntityCollection[] = [
-		examCollection,
-		productsCollection,
-		usersCollection,
-		blogCollection,
-	];
+  const navigation: EntityCollection[] = [
+    examCollection,
+    productsCollection,
+    usersCollection,
+    blogCollection,
+  ];
 
-	const additionalViews: AdditionalView[] = [
-		{
-			path: "additional",
-			name: "Additional",
-			group: "Content",
-			view: <UploadQuestionsView />,
-		},
-	];
+  const additionalViews: AdditionalView[] = [
+    {
+      path: "upload-data",
+      name: "CSV Uploader",
+      group: "Testing",
+      view: <UploadQuestionsView />,
+    },
+  ];
 
-	const myAuthenticator: Authenticator = (user?: firebase.User) => {
-		// Check if the user is someone we want to manage CMS
-		if (
-			user?.email === "jcm.codes@gmail.com" ||
-			user?.email === "ileolagold.olalekan@gmail.com"
-		) {
-			console.log("Allowing access to ", user?.email);
-			return true;
-		}
-		// Otherwise deny access
-		console.log("Not Authorized.");
-		return false;
-	};
+  const myAuthenticator: Authenticator = (user?: firebase.User) => {
+    // Check if the user is someone we want to manage CMS
+    if (
+      user?.email === "jcm.codes@gmail.com" ||
+      user?.email === "ileolagold.olalekan@gmail.com"
+    ) {
+      console.log("Allowing access to ", user?.email);
+      return true;
+    }
+    // Otherwise deny access
+    console.log("Not Authorized.");
+    return false;
+  };
 
-	const customSchemaResolver: SchemaResolver = ({
-		entityId,
-		collectionPath,
-	}: {
-		entityId?: string;
-		collectionPath: string;
-	}) => {
-		if (entityId === "B0017TNJWY" && collectionPath === "products") {
-			const customProductSchema = buildSchema({
-				name: "Custom product",
-				properties: {
-					name: {
-						title: "Name",
-						description:
-							"This entity is using a schema overridden by a schema resolver",
-						validation: { required: true },
-						dataType: "string",
-					},
-				},
-			});
+  const customSchemaResolver: SchemaResolver = ({
+    entityId,
+    collectionPath,
+  }: {
+    entityId?: string;
+    collectionPath: string;
+  }) => {
+    if (entityId === "B0017TNJWY" && collectionPath === "products") {
+      const customProductSchema = buildSchema({
+        name: "Custom product",
+        properties: {
+          name: {
+            title: "Name",
+            description:
+              "This entity is using a schema overridden by a schema resolver",
+            validation: { required: true },
+            dataType: "string",
+          },
+        },
+      });
 
-			console.log("Using custom schema resolver", collectionPath, entityId);
-			return { schema: customProductSchema };
-		}
-	};
+      console.log("Using custom schema resolver", collectionPath, entityId);
+      return { schema: customProductSchema };
+    }
+  };
 
-	const onFirebaseInit = (config: Object) => {
-		// models.firestore().useEmulator("localhost", 8080);
-	};
+  const onFirebaseInit = (config: Object) => {
+    // models.firestore().useEmulator("localhost", 8080);
+  };
 
-	return (
-		<CMSApp
-			name={"Examiner CMS"}
-			authentication={myAuthenticator}
-			// signInOptions={[
-			// 	firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-			// 	firebase.auth.EmailAuthProvider.PROVIDER_ID,
-			// ]}
-			primaryColor="#047857"
-			secondaryColor="#ffd000"
-			allowSkipLogin={false}
-			logo={logo}
-			navigation={navigation}
-			additionalViews={additionalViews}
-			schemaResolver={customSchemaResolver}
-			// In the production environment, the configuration is fetched from the environment automatically
-			firebaseConfig={firebaseConfig}
-			// firebaseConfig={process.env.NODE_ENV !== "production" ? firebaseConfig : undefined}
-			onFirebaseInit={onFirebaseInit}
-		/>
-	);
+  return (
+    <CMSApp
+      name={"Examiner CMS"}
+      authentication={myAuthenticator}
+      // signInOptions={[
+      // 	firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // 	firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      // ]}
+      primaryColor="#047857"
+      secondaryColor="#ffd000"
+      allowSkipLogin={false}
+      logo={logo}
+      navigation={navigation}
+      additionalViews={additionalViews}
+      schemaResolver={customSchemaResolver}
+      // In the production environment, the configuration is fetched from the environment automatically
+      firebaseConfig={firebaseConfig}
+      // firebaseConfig={process.env.NODE_ENV !== "production" ? firebaseConfig : undefined}
+      onFirebaseInit={onFirebaseInit}
+    />
+  );
 }
 
 export default App;
--- cms/src/FileUploadView.tsx
+++ cms/src/FileUploadView.tsx
@@ -1,11 +1,60 @@
-import React, { Component } from "react";
+import { useEffect, useState } from "react";
 import { CSVReader } from "react-papaparse";
-import { useAuthContext } from "@camberi/firecms";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import { firebaseConfig } from "./firebase_config";
 
 export const UploadQuestionsView = () => {
-  const auth = useAuthContext();
+  const someTest = "Hello";
+  const [exams, setExams] = useState({ name: "" });
+  let optionsList: any = [];
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  const db = firebase.firestore();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const getExams = async () => {
+    try {
+      const data = await db.collection("exams").get();
+      console.log("Exams");
+      console.log(data.docs[0].id);
+      let id = data.docs[0].id;
+      const docRef = db.collection("exams").doc(id);
+      const doc = await docRef.get();
+      if (!doc.exists) {
+        console.log("No such document!");
+      } else {
+        let data: any = doc.data();
+
+        console.log("Document data:", doc.data());
+        setExams(data);
+        return data;
+      }
+    } catch (error) {
+      return { error };
+    }
+  };
+
+  const createRecord = async (data: any, exam: any) => {
+    try {
+      await db
+        .collection("exams")
+        .doc(exam.id)
+        .collection("questions")
+        .doc(data.id)
+        .set(data);
+      console.log("data created");
+    } catch (error) {
+      return { error };
+    }
+  };
 
   const handleOnDrop = (data: any) => {
+    getExams();
     console.log("---------------------------");
     console.log(data);
     console.log("---------------------------");
@@ -21,6 +70,15 @@ export const UploadQuestionsView = () => {
     console.log("---------------------------");
   };
 
+  useEffect(() => {
+    const update = () => {
+      console.log("effect");
+      getExams();
+    };
+
+    update();
+  }, []);
+
   return (
     <>
       <div style={{ maxWidth: "900px", margin: "0 auto", padding: "12em" }}>
@@ -29,13 +87,27 @@ export const UploadQuestionsView = () => {
           <h2>Upload Questions for an exam</h2>
         </div>
         <div>
+          <h3 style={{ fontSize: "1.4em" }} className="">
+            Exams
+          </h3>
+          <ul>
+            <li>{exams ? exams.name : "loading"}</li>
+          </ul>
+        </div>
+        <div>
           <h5>Click and Drag Upload</h5>
           <CSVReader
             onDrop={handleOnDrop}
             onError={handleOnError}
             addRemoveButton
             onRemoveFile={handleOnRemoveFile}>
-            <span>Drop CSV file here or click to upload.</span>
+            <div
+              style={{
+                height: "100px",
+                paddingTop: "40px",
+              }}>
+              <span>Drop CSV file here or click to upload.</span>
+            </div>
           </CSVReader>
         </div>
       </div>
