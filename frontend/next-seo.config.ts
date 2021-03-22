aa0c39d96f203bcdd25267134c57e6056558f485
John McDonald <jmcdonald1991@gmail.com>
2021-03-21 20:04:27 -0500

    og image app

--- frontend/next-seo.config.ts
+++ frontend/next-seo.config.ts
@@ -3,4 +3,21 @@ export default {
   defaultTitle: "Examiner Exam Prep",
   description: "Prepare and take practice exams for UTME and more.",
   canonical: "https://www.canonical.ie/",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.url.ie/",
+    site_name: "Examiner Test Preparation for Students",
+    description:
+      "Examiner gives Nigerian students access to the best in computer assisted learning.",
+    images: [
+      {
+        url:
+          "https://firebasestorage.googleapis.com/v0/b/examiner-app-3b4f8.appspot.com/o/site%2Fclasses-1.jpg?alt=media&token=aa750442-b546-4e8b-90e6-1600507b986b",
+        width: 800,
+        height: 600,
+        alt: "Nigerian Exam Preparation CBT",
+      },
+    ],
+  },
 };
